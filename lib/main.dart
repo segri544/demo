dad4041b69b463f3999e243201ee765c67f16f02
Sadık EĞRİ <segri544@gmail.com>
2023-07-22 13:14:23 +0300

    trying

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_mao/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+// try commit
 void main() {
   runApp(const MyApp());
 }
