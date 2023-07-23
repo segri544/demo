06f0c16dba9f49c12bee2b09a48b9f0aa045b836
Sadık EĞRİ <segri544@gmail.com>
2023-07-23 12:26:15 +0300

    fetch tamam harita ekle

--- lib/screens/track_page.dart
+++ lib/screens/track_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrackPage extends StatelessWidget {
   final String documentId;
@@ -36,49 +37,17 @@ class TrackPage extends StatelessWidget {
                 if (index == 0 && hasMorningData) {
                   // Display morning data
                   final morningData = data["sabah"] as Map<String, dynamic>;
-                  return ListTile(
-                    title: Text("Morning"),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Name: ${morningData['name']}"),
-                        Text("Driver Name: ${morningData['driverName']}"),
-                        Text("Phone: ${morningData['phone']}"),
-                        // Add more fields as needed
-                      ],
-                    ),
-                  );
+                  return _buildListTile("Morning", morningData);
                 } else if (index == 1 && hasEveningData) {
                   // Display evening data
                   final eveningData = data["akşam"] as Map<String, dynamic>;
-                  return ListTile(
-                    title: Text("Evening"),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Name: ${eveningData['name']}"),
-                        Text("Driver Name: ${eveningData['driverName']}"),
-                        Text("Phone: ${eveningData['phone']}"),
-                        // Add more fields as needed
-                      ],
-                    ),
-                  );
+                  return _buildListTile("Evening", eveningData);
                 } else {
                   // If only morning or evening data is present, display that data
                   final singleData = data.values.first as Map<String, dynamic>;
-                  return ListTile(
-                    title: Text(singleData.containsKey('sabah')
-                        ? "Morning"
-                        : "Evening"),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Name: ${singleData['name']}"),
-                        Text("Driver Name: ${singleData['driverName']}"),
-                        Text("Phone: ${singleData['phone']}"),
-                        // Add more fields as needed
-                      ],
-                    ),
+                  return _buildListTile(
+                    singleData.containsKey('sabah') ? "Morning" : "Evening",
+                    singleData,
                   );
                 }
               },
@@ -86,6 +55,41 @@ class TrackPage extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+
+  Widget _buildListTile(String title, Map<String, dynamic> data) {
+    return Card(
+      child: ListTile(
+        title: Text(title),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Name: ${data['name']}"),
+            Text("Driver Name: ${data['driverName']}"),
+            Text("Phone: ${data['phone']}"),
+            _buildGeoPointInfo(data['locations']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGeoPointInfo(List<dynamic>? locations) {
+    if (locations == null || locations.isEmpty) {
+      return Text("No GeoPoints available");
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("GeoPoints:"),
+        for (var location in locations)
+          if (location is GeoPoint)
+            Text(
+              "Latitude: ${location.latitude}, Longitude: ${location.longitude}",
+            ),
+      ],
     );
   }
 }
